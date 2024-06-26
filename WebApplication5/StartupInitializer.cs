dc576108a2a20f558df91c1b2ce84c0df6fd451c
MaxZiz <maximz2517@yandex.ru>
2024-06-26 14:12:41 +0500

    Delete Unused Files

---
+++ WebApplication5/StartupInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WMS.DAL.Interfaces;
+using WMS.DAL.Repositories;
+using WMS.Domain.Entities;
+using WMS.Service.Implementations;
+using WMS.Service.Interfaces;
+
+namespace WMS
+{
+    public static class StartupInitializer
+    {
+        public static void InitializeRepositories(this IServiceCollection services)
+        {              
+        }
+
+        public static void InitializeServices(this IServiceCollection services)
+        {               
+        }
+    }
+}
